3c8240e89f48d43a529f0f4254e967fa31ce3c18
TuJuicio <kevinmoll.ls@gmail.com>
2022-01-25 07:18:27 -0400

    changes

--- express/server.js
+++ express/server.js
@@ -12,17 +12,20 @@ const run = async (url) => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
-  // set the size of the viewport, so our screenshot will have the desired size
   await page.setViewport({
-    width: 1280,
-    height: 800
+      width: 1280,
+      height: 800
   })
 
   await page.goto(url)
-  let capture = await page.screenshot({type: 'png'})
-  await browser.close();
-  console.log('run');
 
+  let capture = await page.evaluate(() => {
+      const canvas = document.querySelector(".printable canvas");
+      return canvas.toDataURL('image/png');
+  });
+
+  //let capture = await page.screenshot({type: 'png'})
+  await browser.close();
   return capture;
 };
 
@@ -32,29 +35,72 @@ router.get('/', (req, res) => {
   res.end();
 });
 
-app.get('/v-a4', function(req, res){
+router.get('/v-a4', (req, res) => {
   let user = req.query.user;
-  run('https://rpay.app/'+user).then(function(source){
-      res.writeHead(200, {
-          'Content-Type': 'image/png',
-          'Content-Length': source.length
-      });
-      res.end(source);
-  })
-});
-
-router.get('/va4', (req, res) => {
-  run('https://rpay.app/r3zu3').then(function(source){
-    res.writeHead(200, {
-      'Content-Type': 'image/png',
-      'Content-Length': source.length
-    });
-    res.end(source);
+  run('https://rpay.app/'+user+'?print=v-a4').then(function(source){
+    res.json({ imgdate: source })
   })
 });
 
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+router.get('/h-a4', function(req, res){
+  let user = req.query.user;
+  run('https://rpay.app/'+user+'?print=h-a4').then(function(source){
+    res.json({ imgdate: source })
+  })
+});
+
+router.get('/v-a5', function(req, res){
+  let user = req.query.user;
+  run('https://rpay.app/'+user+'?print=v-a5').then(function(source){
+    res.json({ imgdate: source })
+  })
+});
+
+router.get('/h-a5', function(req, res){
+  let user = req.query.user;
+  run('https://rpay.app/'+user+'?print=h-a5').then(function(source){
+    res.json({ imgdate: source })
+  })
+});
+
+router.get('/v-a6', function(req, res){
+  let user = req.query.user;
+  run('https://rpay.app/'+user+'?print=v-a6').then(function(source){
+    res.json({ imgdate: source })
+  })
+});
+
+router.get('/h-a6', function(req, res){
+  let user = req.query.user;
+  run('https://rpay.app/'+user+'?print=h-a6').then(function(source){
+    res.json({ imgdate: source })
+  })
+});
+
+router.get('/igf', function(req, res){
+  let user = req.query.user;
+  run('https://rpay.app/'+user+'?print=igf').then(function(source){
+    res.json({ imgdate: source })
+  })
+});
+
+router.get('/igs', function(req, res){
+  let user = req.query.user;
+  run('https://rpay.app/'+user+'?print=igs').then(function(source){
+    res.json({ imgdate: source })
+  })
+});
+
+router.get('/dmc', function(req, res){
+  let user = req.query.user;
+  run('https://rpay.app/'+user+'?print=dmc').then(function(source){
+    res.json({ imgdate: source })
+  })
+});
+
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
--- server-local.js
+++ server-local.js
@@ -8,116 +8,83 @@ app.listen(3000, () => console.log('Local app listening on port 3000!'));
 const run = async (url) => {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-  
-    // set the size of the viewport, so our screenshot will have the desired size
+
     await page.setViewport({
         width: 1280,
         height: 800
     })
   
     await page.goto(url)
-    let capture = await page.screenshot({type: 'png'})
-    await browser.close();
-    console.log('run');
 
+    let capture = await page.evaluate(() => {
+        const canvas = document.querySelector(".printable canvas");
+        return canvas.toDataURL('image/png');
+    });
+
+    //let capture = await page.screenshot({type: 'png'})
+    await browser.close();
     return capture;
 };
 
 app.get('/v-a4', function(req, res){
     let user = req.query.user;
-    run('https://rpay.app/'+user).then(function(source){
-        res.writeHead(200, {
-            'Content-Type': 'image/png',
-            'Content-Length': source.length
-        });
-        res.end(source);
+    run('https://rpay.app/'+user+'?print=v-a4').then(function(source){
+        res.json({ imgdate: source })
     })
 });
 
 app.get('/h-a4', function(req, res){
     let user = req.query.user;
-    run('https://rpay.app/'+user).then(function(source){
-        res.writeHead(200, {
-            'Content-Type': 'image/png',
-            'Content-Length': source.length
-        });
-        res.end(source);
+    run('https://rpay.app/'+user+'?print=h-a4').then(function(source){
+        res.json({ imgdate: source })
     })
 });
 
 app.get('/v-a5', function(req, res){
     let user = req.query.user;
-    run('https://rpay.app/'+user).then(function(source){
-        res.writeHead(200, {
-            'Content-Type': 'image/png',
-            'Content-Length': source.length
-        });
-        res.end(source);
+    run('https://rpay.app/'+user+'?print=v-a5').then(function(source){
+        res.json({ imgdate: source })
     })
 });
 
 app.get('/h-a5', function(req, res){
     let user = req.query.user;
-    run('https://rpay.app/'+user).then(function(source){
-        res.writeHead(200, {
-            'Content-Type': 'image/png',
-            'Content-Length': source.length
-        });
-        res.end(source);
+    run('https://rpay.app/'+user+'?print=h-a5').then(function(source){
+        res.json({ imgdate: source })
     })
 });
 
 app.get('/v-a6', function(req, res){
     let user = req.query.user;
-    run('https://rpay.app/'+user).then(function(source){
-        res.writeHead(200, {
-            'Content-Type': 'image/png',
-            'Content-Length': source.length
-        });
-        res.end(source);
+    run('https://rpay.app/'+user+'?print=v-a6').then(function(source){
+        res.json({ imgdate: source })
     })
 });
 
 app.get('/h-a6', function(req, res){
     let user = req.query.user;
-    run('https://rpay.app/'+user).then(function(source){
-        res.writeHead(200, {
-            'Content-Type': 'image/png',
-            'Content-Length': source.length
-        });
-        res.end(source);
+    run('https://rpay.app/'+user+'?print=h-a6').then(function(source){
+        res.json({ imgdate: source })
     })
 });
 
 app.get('/igf', function(req, res){
     let user = req.query.user;
-    run('https://rpay.app/'+user).then(function(source){
-        res.writeHead(200, {
-            'Content-Type': 'image/png',
-            'Content-Length': source.length
-        });
-        res.end(source);
+    run('https://rpay.app/'+user+'?print=igf').then(function(source){
+        res.json({ imgdate: source })
     })
 });
 
 app.get('/igs', function(req, res){
     let user = req.query.user;
-    run('https://rpay.app/'+user).then(function(source){
-        res.writeHead(200, {
-            'Content-Type': 'image/png',
-            'Content-Length': source.length
-        });
-        res.end(source);
+    run('https://rpay.app/'+user+'?print=igs').then(function(source){
+        res.json({ imgdate: source })
     })
 });
 
 app.get('/dmc', function(req, res){
     let user = req.query.user;
-    run('https://rpay.app/'+user).then(function(source){
-        res.writeHead(200, {
-            'Content-Type': 'image/png',
-            'Content-Length': source.length
-        });
-        res.end(source);
+    run('https://rpay.app/'+user+'?print=dmc').then(function(source){
+        res.json({ imgdate: source })
     })
 });
